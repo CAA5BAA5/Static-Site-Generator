0560dba16ee657f457d03e27932c07ecde68f20f
CAA5BAA5 <mavadebo@proton.me>
2025-03-16 10:42:52 +0100

    added link and image logic for markdown

--- src/markdown.py
+++ src/markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     if not isinstance(old_nodes, list):
@@ -7,6 +8,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     count = 1
     new_nodes = []
     for node in old_nodes:
+        if isinstance(node, TextNode):
+            node = node.text
         for segment in node.split(delimiter):
             if count % 2 == 1:
                 new_nodes.append(TextNode(segment, TextType.TEXT))
@@ -15,3 +18,35 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             count +=1
     
     return new_nodes
+
+def split_nodes_on_media_regex(old_nodes, regex, text_type):
+    if not isinstance(old_nodes, list):
+        raise TypeError("old_nodes must be a list")
+    
+    count = 1
+    new_nodes = []
+    for node in old_nodes:
+        if isinstance(node, TextNode):
+            node = node.text
+        for segment in re.split(regex, node):
+            if count % 2 == 1:
+                if len(segment) > 0:
+                    new_nodes.append(TextNode(segment, TextType.TEXT))
+            elif count % 2 == 0:
+                sep = segment.split("](")
+                new_nodes.append(TextNode(sep[0], text_type, sep[1]))
+            count +=1
+    
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    return split_nodes_on_media_regex(old_nodes, r"!\[([^\[\]]*\]\([^\(\)]*)\)", TextType.IMAGE)
+
+def split_nodes_link(old_nodes):
+    return split_nodes_on_media_regex(old_nodes, r"(?<!!)\[([^\[\]]*\]\([^\(\)]*)\)", TextType.LINK)
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,6 +1,5 @@
 import unittest
-
-from markdown import split_nodes_delimiter
+import markdown
 from textnode import TextNode, TextType
 
 class TestMarkdown(unittest.TestCase):
@@ -11,7 +10,7 @@ class TestMarkdown(unittest.TestCase):
             TextNode("code block", TextType.CODE),
             TextNode(" word", TextType.TEXT)
         ]
-        self.assertEqual(split_nodes_delimiter([sample_text], "`", TextType.CODE), expected)
+        self.assertEqual(markdown.split_nodes_delimiter([sample_text], "`", TextType.CODE), expected)
 
         sample_text = "This is text with a <italic block< word"
         expected = [
@@ -19,7 +18,91 @@ class TestMarkdown(unittest.TestCase):
             TextNode("italic block", TextType.ITALIC),
             TextNode(" word", TextType.TEXT)
         ]
-        self.assertEqual(split_nodes_delimiter([sample_text], "<", TextType.ITALIC), expected)
+        self.assertEqual(markdown.split_nodes_delimiter([sample_text], "<", TextType.ITALIC), expected)
+
+    def test_extract_images(self):
+        matches = markdown.extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+        matches = markdown.extract_markdown_images(
+            "This is text with an ![picture](../content/header.jpeg)"
+        )
+        self.assertListEqual([("picture", "../content/header.jpeg")], matches)
+
+    def test_extract_links(self):
+        matches = markdown.extract_markdown_links(
+            "This is text with an [hyperlink](https://google.com)"
+        )
+        self.assertListEqual([("hyperlink", "https://google.com")], matches)
+
+        matches = markdown.extract_markdown_links(
+            "This is text with an [local link](../public/contact.html)"
+        )
+        self.assertListEqual([("local link", "../public/contact.html")], matches)
+
+    def test_split_nodes_images(self):
+        node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+        )
+        new_nodes = markdown.split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+        #node as raw text
+        node = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)"
+        new_nodes = markdown.split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_nodes_linkss(self):
+        node = TextNode(
+         "This is text with a [hyperlink](https://google.com) and this is text with a [local link](../public/contact.html)",
+        TextType.TEXT,
+        )
+        new_nodes = markdown.split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("hyperlink", TextType.LINK, "https://google.com"),
+                TextNode(" and this is text with a ", TextType.TEXT),
+                TextNode("local link", TextType.LINK, "../public/contact.html"
+                ),
+            ],
+            new_nodes,
+        )
+
+        #node as raw text
+        node = "This is text with a [hyperlink](https://google.com) and this is text with a [local link](../public/contact.html)"
+        new_nodes = markdown.split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("hyperlink", TextType.LINK, "https://google.com"),
+                TextNode(" and this is text with a ", TextType.TEXT),
+                TextNode("local link", TextType.LINK, "../public/contact.html"
+                ),
+            ],
+            new_nodes,
+        )
 
     if __name__ == "__main__":
         unittest.main()
